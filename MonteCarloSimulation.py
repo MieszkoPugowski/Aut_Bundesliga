acb456bbdb524cafaba9121fede18f6501f763cf
Mieszko Pugowski <mpugowski@student.agh.edu.pl>
2025-05-30 18:38:29 +0200

    update: MonteCarloSimulation now can simulate a season

--- MonteCarloSimulation.py
+++ MonteCarloSimulation.py
@@ -3,6 +3,8 @@ from elo_ratings import BundesligaElo
 from random_fixture import CreateFixture
 from typing import Dict
 import time
+from collections import defaultdict
+import pandas as pd
 
 season_dict = CreateFixture().create_gameweeks_dict()
 elo = BundesligaElo()
@@ -10,12 +12,11 @@ elo = BundesligaElo()
 league_dict = {}
 for team in elo.ratings.keys():
     league_dict[team] = 0
-start_time = time.time()
 class MonteCarloSim:
     def __init__(self,n:int=1):
         self.n = n
 
-    def simulate_one_game(self,home,away,n=1):
+    def simulate_one_game(self,home,away):
         assert home,away in elo.ratings.keys()
         result_probs = {"home":0,"draw":0,"away":0}
         rand = random.random()
@@ -28,3 +29,30 @@ class MonteCarloSim:
             result_probs["away"] += 1
         return result_probs
 
+    def simulate_season(self, season_fixture:Dict=season_dict):
+        position_counts = defaultdict(lambda: defaultdict(int))
+        for i in range(0,self.n):
+            # Reset league points and elo ratings for each simulation
+            league_dict = defaultdict(int)
+            elo = BundesligaElo()
+            for gameweek in season_fixture:
+                for game in season_fixture[gameweek]:
+                    result = self.simulate_one_game(game[0],game[1])
+                    exp_win = elo.match_result_probabilities(game[0],game[1])["home_win"]
+                    if result["home"]:
+                        league_dict[game[0]] += 3
+                        elo.update_elo_ratings(game[0], game[1], 1, exp_win)
+                    elif result["away"]:
+                        league_dict[game[1]] += 3
+                        elo.update_elo_ratings(game[0], game[1], 0, exp_win)
+                    else:
+                        league_dict[game[0]] += 1
+                        league_dict[game[1]] += 1
+                        elo.update_elo_ratings(game[0], game[1], 0.5, exp_win)
+            standings = sorted(league_dict.items(),key=lambda x:x[1],reverse=True)
+            for position, (team, _) in enumerate(standings, 1):
+                position_counts[team][position] += 1
+        df = pd.DataFrame().from_dict(position_counts,orient="index")
+        df = df.reindex(sorted(df.columns), axis=1)
+        # Sort by most 1st places, then 2nd places, etc.
+        df = df.sort_values(
